62687c9daab9cfa20dd62d457df91118fcef42c0
Daniel Hammer <daniel.hammer+oss@gmail.com>
2025-12-19 18:20:36 +0100

    Spring Boot 4.0 symptom

--- src/main/java/reproducer/logger/SpringAdminLogger.java
+++ src/main/java/reproducer/logger/SpringAdminLogger.java
@@ -1,12 +1,11 @@
 package reproducer.logger;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 import reproducer.model.AdminLog;
+import tools.jackson.databind.json.JsonMapper;
 
 @Component
 @ConditionalOnProperty(name = "custom-admin-log.enabled", havingValue = "true")
@@ -14,20 +13,16 @@ public class SpringAdminLogger {
 
     private Logger log = LoggerFactory.getLogger(SpringAdminLogger.class);
 
-    private final ObjectMapper objectMapper;
+    private final JsonMapper jsonMapper;
 
-    public SpringAdminLogger(ObjectMapper objectMapper) {
-        this.objectMapper = objectMapper;
+    public SpringAdminLogger(JsonMapper jsonMapper) {
+        this.jsonMapper = jsonMapper;
     }
 
     public void trace(AdminLog adminLog) {
         log.info("Tracing: {}", adminLog);
 
-        try {
-            AdminLogger.trace(objectMapper.writeValueAsString(adminLog));
-        } catch (JsonProcessingException e) {
-            log.error("Error writing: {}", adminLog, e);
-        }
+        AdminLogger.trace(jsonMapper.writeValueAsString(adminLog));
     }
 
 }
